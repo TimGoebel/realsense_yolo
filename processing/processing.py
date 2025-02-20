296ed6ba50110cfa1b99b7c1dcd6c02fbcf9338b
TimGoebel <100859124+TimGoebel@users.noreply.github.com>
2025-02-20 14:12:06 -0600

    Add files via upload

--- processing/processing.py
+++ processing/processing.py
@@ -1,1 +1,22 @@
+# processing.py
+import cv2
+import numpy as np
+import queue
+from utils.utils import crop_zone_process
 
+def process_frames(model, set_points, depth_scale, stop_event, frame_queue, result_queue):
+    while not stop_event.is_set():
+        try:
+            color_image, depth_colormap, intrinsics, depth_frame_aligned1 = frame_queue.get(timeout=0.1)
+        except queue.Empty:
+            continue
+
+        processed_color, processed_depth = crop_zone_process(
+            color_image, depth_colormap, model, set_points,
+            depth_frame_aligned1, depth_scale, intrinsics
+        )
+        images = np.hstack((processed_color, processed_depth))
+        try:
+            result_queue.put(images, timeout=0.1)
+        except queue.Full:
+            pass
